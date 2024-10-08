f7a04851815347bb15b58d924acd819ec718684d
davesalvador <101625754+davesalvador@users.noreply.github.com>
2024-10-08 11:32:20 +0800

    Added Features

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/LandingPage/Header';
 import Hero from '../components/LandingPage/Hero';
+import Features from '../components/LandingPage/Features';
 import '../styles/landing_page.css'
 import { PlanningAndDevelopmentOffice } from '../assets/images';
 
@@ -15,8 +16,11 @@ const HomePage = () => {
         <img src={PlanningAndDevelopmentOffice} alt="Logo" />
         <div className="rectangle"></div>
       </div>
-      <div className="container"> <Header /> 
+      <div className="container">
+        
+      <Header /> 
       <Hero />
+      <Features />
       </div>
      
       {/* <div>
