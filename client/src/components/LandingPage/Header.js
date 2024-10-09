ef5a58a5b5bf9eb0b01dd78bf105c597f144a8aa
davesalvador <101625754+davesalvador@users.noreply.github.com>
2024-10-09 12:28:15 +0800

    Update Header.js
    Added the shadow effect when scrolling

--- client/src/components/LandingPage/Header.js
+++ client/src/components/LandingPage/Header.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { BulSULogo, OPCRLogo } from '../../assets/images'
-import '../../styles/landing_page.css'
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { BulSULogo, OPCRLogo } from '../../assets/images';
+import '../../styles/landing_page.css';
 
 function Header() {
-
-    const [showNavBarShadow, setShowNavBarShadow] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [showNavBarShadow, setShowNavBarShadow] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [scrollY, setScrollY] = useState(0); // State to track the scroll position
 
   // Scroll to FAQs section
   const scrollToFAQsSection = () => {
@@ -19,22 +18,35 @@ function Header() {
     }
   };
 
-    // Scroll to Contact section
-    const scrollToContactSection = () => {
-        if (faqsSectionRef.current) {
-          window.scrollTo({
-            top: contactSectionRef.current.offsetTop,
-            behavior: "smooth",
-          });
-        }
-      };
+  // Scroll to Contact section
+  const scrollToContactSection = () => {
+    if (faqsSectionRef.current) {
+      window.scrollTo({
+        top: contactSectionRef.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
 
-        // Togggle menu
+  // Toggle menu
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  // Handle scroll animation for logos and shadow for the navbar
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowNavBarShadow(window.scrollY >= 100); // Show shadow after scrolling 100px
+      setScrollY(window.scrollY); // Track current scroll position
+    };
 
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <header
@@ -78,8 +90,7 @@ function Header() {
         </Link>
       </div>
     </header>
-
-  )
+  );
 }
 
-export default Header
+export default Header;
